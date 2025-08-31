4d36d6e11b00dcb7c232263b1bcba10b3540b99b
Matthew Teelucksingh <teelucksingh.matthew1@gmail.com>
2025-08-31 04:44:41 -0700

    Vertical video template

--- src/vertical_video/views.py
+++ src/vertical_video/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 
-# Create your views here.
+from vertical_video.models import VerticalVideo
+
 def index(request):
-    return render(request, "index.html", {})
+
+    all_videos = VerticalVideo.objects.all()
+
+    return render(request, "index.html", {"videos": all_videos})
